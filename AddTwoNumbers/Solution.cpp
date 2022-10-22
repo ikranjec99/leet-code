91e33af553f188e7e14fd5b5391bdbf271b4af2c
Ivan <ivan.kranjec95@gmail.com>
2022-10-23 00:09:50 +0200

    [AddTwoNumbers] - add first part of solution.

---
+++ AddTwoNumbers/Solution.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Solution.h"
+
+Solution::Solution() { }
+
+ListNode* Solution::addTwoNumbers(ListNode* l1, ListNode* l2) 
+{
+	return nullptr;
+}
+
+
---
+++ AddTwoNumbers/Solution.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "ListNode.h"
+
+class Solution 
+{
+public:
+	Solution();
+
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2);
+};
