e44ac387dc3fdf92eed8e8bba96afc4c0fff7629
Ivan <ivan.kranjec95@gmail.com>
2022-10-22 23:47:42 +0200

    [TwoSum] - add example to main cpp file.

--- TwoSum/TwoSum.cpp
+++ TwoSum/TwoSum.cpp
@@ -3,5 +3,12 @@
 
 int main()
 {
+    Solution solution;
+    
+    std::vector<int> values = { 3, 2, 4 };
+    int target = 6;
+    
+    std::vector<int> vector = solution.twoSum(values, target);
 
+    solution.printResults(vector);
 }
