9d5fc72b7e9c1725d55d0f87251cfbf2f7c48e9d
Ivan <ivan.kranjec95@gmail.com>
2022-10-24 00:05:02 +0200

    [AddTwoNumbers] - add solution.

--- AddTwoNumbers/Solution.cpp
+++ AddTwoNumbers/Solution.cpp
@@ -5,7 +5,28 @@ Solution::Solution() { }
 
 ListNode* Solution::addTwoNumbers(ListNode* l1, ListNode* l2) 
 {
-	return nullptr;
+	ListNode head(0);
+	ListNode* current = &head;
+
+	int carry = 0;
+
+	while (l1 || l2 || carry) 
+	{
+		int x = l1 ? l1->val : 0;
+		int y = l2 ? l2->val : 0;
+
+		ListNode* node = new ListNode((x + y + carry) % 10);
+
+		current->next = node;
+		current = node;
+
+		carry = (x + y + carry) / 10;
+
+		if (l1) l1 = l1->next;
+		if (l2) l2 = l2->next;
+	}
+
+	return head.next;
 }
 
 
