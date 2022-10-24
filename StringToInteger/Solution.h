e3cf3c76533f8a39b0431f18c50d9f5a8e0bb103
Ivan <ivan.kranjec95@gmail.com>
2022-10-25 00:05:14 +0200

    [StringToInteger] - add Solution header.

---
+++ StringToInteger/Solution.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+class Solution 
+{
+public:
+	Solution();
+
+	bool anyCharRepresentsNegativeNumber(std::string s);
+	int charToInt(char c);
+	bool isCharNumber(char c);
+	int myAtio(std::string s);
+	int numericalCharactersLength(std::string s);
+};
