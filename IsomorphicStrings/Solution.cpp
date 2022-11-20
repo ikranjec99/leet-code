182b4d8f59d21d094e4eae721dd0a7bb7eedcca6
Ivan <ivan.kranjec95@gmail.com>
2022-11-20 15:04:45 +0100

    [IsomorphicStrings] - add template.

---
+++ IsomorphicStrings/Solution.cpp
@@ -0,0 +1,8 @@
+#include "Solution.h"
+
+Solution::Solution() { };
+
+bool Solution::isIsomorphic(std::string s, std::string t)
+{
+
+}
---
+++ IsomorphicStrings/Solution.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+class Solution
+{
+public:
+	Solution();
+
+	bool isIsomorphic(std::string s, std::string t);
+};
