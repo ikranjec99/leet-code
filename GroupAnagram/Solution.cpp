ffd3416e03ec2dcc33bd66c9f0bdbef7bc2ecb72
Ivan <ivan.kranjec95@gmail.com>
2022-12-04 23:39:41 +0100

    [GroupAnagran] - add template for solution.

---
+++ GroupAnagram/Solution.cpp
@@ -0,0 +1,9 @@
+#include "Solution.h"
+
+Solution::Solution() { }
+
+std::vector<std::vector<std::string>> Solution::groupAnagrams(std::vector<std::string>& strs)
+{
+
+};
+
---
+++ GroupAnagram/Solution.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+	Solution();
+
+	std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs);
+};
